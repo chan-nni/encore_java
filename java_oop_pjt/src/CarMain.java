27a5bf35ade9bb1e6a888022cbbfee2db1a2aa07
chan-nni <rkdcksal02@gmail.com>
2024-01-30 17:35:29 +0900

    java_oop_20241030

---
+++ java_oop_pjt/src/CarMain.java
@@ -0,0 +1,16 @@
+import com.encore.car.Car;
+
+// https://github.com/jslim9413/git_encore_java.git
+
+public class CarMain {
+	
+	public static void main(String[] args) {
+		
+		Car car = new Car() ;
+		// Car car = new Car("가솔린");
+		car.engineInfo();
+		
+	}
+}
+
+
