b93f1ee14d733bf7927e1b83969396f7693bafd6
chan-nni <rkdcksal02@gmail.com>
2024-02-08 22:49:47 +0900

    20240208_파일 출력

--- java_oop_pjt/src/IODemoMain.java
+++ java_oop_pjt/src/IODemoMain.java
@@ -40,22 +40,37 @@ public class IODemoMain {
 //		System.out.println(">>>> main end <<<<");
 		
 		// case 04.
-		String msg = "aBc";
-		System.out.println(msg.equals("abc"));
-		System.out.println(msg.equalsIgnoreCase("abc")); // 대소문자 구분하지 않고 비교
-		System.out.println(msg.toUpperCase()); // 대문자 변환
-		System.out.println(msg.toLowerCase()); // 소문자 변환
-		System.out.println(msg.length()); // 문자 길이
-		System.out.println(msg.contains("B")); // 포함하는지 비교, 대소문자 구분함
-		System.out.println(msg.startsWith("a")); // 시작하는 문자 비교
-		System.out.println(msg.endsWith("c")); // 끝나는 문자 비교
-		msg = "a b c";
-		System.out.println(msg.trim());
-		System.out.println(msg.trim().length()); // 공백 포함 문자 길이
-		msg = "abcdef";
-		System.out.println(msg.charAt(1)); // 원하는 문자 가져옴
-		System.out.println(msg.substring(2)); // 2번째 인덱스 번호부터 가져옴
-		System.out.println(msg.substring(2, 5)); // substring에 마지막 인덱스는 -1이 들어간다. 그래서 cde 출력
+//		String msg = "aBc";
+//		System.out.println(msg.equals("abc"));
+//		System.out.println(msg.equalsIgnoreCase("abc")); // 대소문자 구분하지 않고 비교
+//		System.out.println(msg.toUpperCase()); // 대문자 변환
+//		System.out.println(msg.toLowerCase()); // 소문자 변환
+//		System.out.println(msg.length()); // 문자 길이
+//		System.out.println(msg.contains("B")); // 포함하는지 비교, 대소문자 구분함
+//		System.out.println(msg.startsWith("a")); // 시작하는 문자 비교
+//		System.out.println(msg.endsWith("c")); // 끝나는 문자 비교
+//		msg = "a b c";
+//		System.out.println(msg.trim());
+//		System.out.println(msg.trim().length()); // 공백 포함 문자 길이
+//		msg = "abcdef";
+//		System.out.println(msg.charAt(1)); // 원하는 문자 가져옴
+//		System.out.println(msg.substring(2)); // 2번째 인덱스 번호부터 가져옴
+//		System.out.println(msg.substring(2, 5)); // substring에 마지막 인덱스는 -1이 들어간다. 그래서 cde 출력
+//		
+		
+		// case 05.
+//		boolean flag = demo.outputFile();
+//		String msg = (flag == true) ? "저장성공" : "저장실패";
+//		System.out.println(msg);
+		
+		// case 06.
+//		boolean flag = demo.outputObjFile();
+//		String msg = (flag == true) ? "저장성공" : "저장실패";
+//		System.out.println(msg);
+		
+		// case 07.
+		// demo.outputSerializable();
+		demo.inputSerializable();
 		
 	}
 
--- java_oop_pjt/src/com/encore/stream/IODemo.java
+++ java_oop_pjt/src/com/encore/stream/IODemo.java
@@ -1,8 +1,19 @@
 package com.encore.stream;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.Scanner;
+
+import com.encore.hms.domain.StudentDTO;
+import com.encore.hms.domain.sup.Person;
 
 public class IODemo {
 	
@@ -24,7 +35,7 @@ public class IODemo {
 		
 		int data = 0;
 	    try {
-	    	data = Integer.parseInt(reader.readLine());
+	    	data = Integer.parseInt(reader.readLine()); // 문자열을 정수로 변환해준다.
 	    
 	    } catch (NumberFormatException e) {
 	    	e.printStackTrace();
@@ -65,9 +76,130 @@ public class IODemo {
 		}
 	}
 	
+	//파일 출력 기능
+	public boolean outputFile() {
+	    boolean flag = false;
+	    Scanner scan = new Scanner(System.in);
+	    System.out.print("Input : ");
+	    String msg = scan.nextLine(); //공백 포함 문자열 nextline()
+	    
+	    //콘솔에서 입력한 데이터를 파일로 저장
+	    File file = new File("C:\\file/testWriter.txt");
+	    FileWriter writer = null;
+	    BufferedWriter bw = null;
+	    try {
+	        writer = new FileWriter(file);
+	        bw = new BufferedWriter(writer);
+	        bw.write(msg);
+	        flag = true;
+	    } catch (Exception e) {
+	        throw new RuntimeException(e);
+	    } finally {
+	        try {
+	        	if(bw != null) {bw.close();} // 값이 있을 때만 close 한다는 뜻
+	        } catch (IOException e) {
+	            throw new RuntimeException(e);
+	        }
+	    }
+	    return flag;
+	}
 	
+	//객체가 가지고 있는 정보를 파일에 저장
+	public boolean outputObjFile() {
+	    boolean flag = false;
+	    
+	    Person [] perAry = new Person[10];
+	    int idx = 0;
+	    perAry[idx++] = new StudentDTO("강찬미", 20, "서울", "2020");
+	    perAry[idx++] = new StudentDTO("강찬미", 20, "서울", "2020");
+	    perAry[idx++] = new StudentDTO("강찬미", 20, "서울", "2020");
+	    
+	    
+	    File file = new File("C:\\file/testWriter.txt");
+	    FileWriter writer = null;
+	    BufferedWriter bw = null;
+	    try {
+	        writer = new FileWriter(file);
+	        bw = new BufferedWriter(writer);
+	        for(int i = 0 ; i < perAry.length ; i++) {
+	        	if(perAry[i] != null) {
+	        	bw.write(perAry[i].getName() + perAry[i].getAge() + ":" + perAry[i].getAddress() + ((StudentDTO)perAry[i]).getStuId());
+	        	bw.newLine();
+	        }
+	    }
+	        
+	        flag = true;
+	    } catch (Exception e) {
+	        throw new RuntimeException(e);
+	    } finally {
+	        try {
+	        	if(bw != null) {bw.close();} // 값이 있을 때만 close 한다는 뜻
+	        } catch (IOException e) {
+	            throw new RuntimeException(e);
+	        }
+	    }
+	    return flag;
+	}
 	
-	
+	public void outputSerializable() {
+		// String msg = "난 직렬화된 객체여서 바이너리 형태로 저장이 가능함.";
+		// StudentDTO stu = new StudentDTO("강찬미", 20 , "강북구", "2020");
+		
+		Person [] perAry = new Person[10];
+	    int idx = 0;
+	    perAry[idx++] = new StudentDTO("강찬미", 20, "서울", "2020");
+	    perAry[idx++] = new StudentDTO("강찬미", 20, "서울", "2020");
+	    perAry[idx++] = new StudentDTO("강찬미", 20, "서울", "2020");
+	    
+		FileOutputStream fis = null;
+		ObjectOutputStream oos = null;
+		try {
+			fis = new FileOutputStream("c:\\file/serial.txt");
+			oos = new ObjectOutputStream(fis);
+			
+			oos.writeObject(perAry);
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			try {
+				if(oos != null) {oos.close();}
+			} catch (Exception e){
+				e.printStackTrace();
+			}
+		}
+	}
+	public void inputSerializable() {
+		String msg = null;
+		FileInputStream fis = null;
+		ObjectInputStream oos = null;
+		try {
+			fis = new FileInputStream("c:\\file/serial.txt");
+			oos = new ObjectInputStream(fis);
+			// msg = (String)(oos.readObject());
+			// StudentDTO stu  = (StudentDTO)(oos.readObject());
+			// System.out.println(stu.personInfo());
+			
+			Person [] perAry = ( Person [] )(oos.readObject());
+			for(int i = 0 ; i < perAry.length; i++) {
+	        	if(perAry[i] != null) {
+	        		System.out.println(perAry[i].personInfo());
+	        	}
+	        }
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			try {
+				if(oos != null) {oos.close();}
+			} catch (Exception e){
+				e.printStackTrace();
+			}
+		}
+	}
+
 	
 	
 }
